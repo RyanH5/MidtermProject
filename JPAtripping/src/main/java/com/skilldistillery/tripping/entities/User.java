c0027e7aec549edb294534f2a34c2599ba1e6014
Ryan Harrington <harrington.ryana@gmail.com>
2019-07-15 15:31:30 -0600

    Stub JPA and BOOT projects

---
+++ JPAtripping/src/main/java/com/skilldistillery/tripping/entities/User.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.tripping.entities;
+
+public class User {
+
+}
---
+++ Tripping/src/main/java/com/skilldistillery/tripping/TrippingApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tripping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippingApplication.class, args);
+	}
+
+}
