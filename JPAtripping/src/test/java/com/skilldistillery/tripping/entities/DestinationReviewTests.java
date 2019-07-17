9860d5f26956a60ce1b6b077e930a1e69c3b7f23
Chana Cohn <chana.cohn@gmail.com>
2019-07-17 13:07:57 -0600

    review tests

---
+++ JPAtripping/src/test/java/com/skilldistillery/tripping/entities/DestinationReviewTests.java
@@ -0,0 +1,60 @@
+package com.skilldistillery.tripping.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class DestinationReviewTests {
+
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("tripping");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+
+	@Disabled
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void test_Destination_review_mappings() {
+		DestinationReview destReview = em.find(DestinationReview.class, 1);
+		assertEquals(1, destReview.getId());
+		assertEquals(2, destReview.getDestination().getId());
+		assertNotNull(destReview.getCreateDate());
+		assertEquals("golden biking review", destReview.getTitle());
+		assertEquals(4, destReview.getRating());
+		assertEquals("it was so awesome i broke my arm", destReview.getReviewText());
+
+	}
+}
