4d4044cbb197ccadd29b2e666a9c45a1dc0950ac
Chana Cohn <chana.cohn@gmail.com>
2019-07-17 10:30:21 -0600

    updating entities

--- JPAtripping/src/main/java/com/skilldistillery/tripping/entities/EventReview.java
+++ JPAtripping/src/main/java/com/skilldistillery/tripping/entities/EventReview.java
@@ -9,12 +9,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
+@Table(name="event_user_review")
 public class EventReview {
 
 //	Declarations
