8cd7dffaf3003498dca836030015706911f62026
Newlifedirection <craig.dreiling@gmail.com>
2019-07-16 21:07:39 -0600

    added @Entity to activity and amentiy

--- JPAtripping/src/main/java/com/skilldistillery/tripping/entities/Activity.java
+++ JPAtripping/src/main/java/com/skilldistillery/tripping/entities/Activity.java
@@ -1,10 +1,12 @@
 package com.skilldistillery.tripping.entities;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Activity {
 
 //	Declarations
