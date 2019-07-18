9856c98f6179f0cd2fa177e2f8d09864e7598141
Ryan Harrington <harrington.ryana@gmail.com>
2019-07-18 11:30:52 -0600

    Implement Admin delete destination

--- Tripping/src/main/java/com/skilldistillery/tripping/data/AdminDAOImpl.java
+++ Tripping/src/main/java/com/skilldistillery/tripping/data/AdminDAOImpl.java
@@ -1,5 +1,113 @@
 package com.skilldistillery.tripping.data;
 
-public class AdminDAOImpl {
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import com.skilldistillery.tripping.entities.*;
 
+@Transactional
+@Service
+public class AdminDAOImpl implements AdminDAO{
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public boolean addDestination(Destination dest) {
+		em.persist(dest);
+		return true;
+	}
+
+	@Override
+	public boolean deleteDestination(Destination dest) {
+		em.remove(dest);
+		return true;
+	}
+
+	@Override
+	public boolean updateDestination(Destination dest) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean addEvent(Event e) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean deleteEvent(Event e) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean updateEvent(Event e) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean addAmenity(Amenity a) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean deleteAmenity(Amenity a) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean updateAmenity(Amenity a) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean addPointOfInterest(PointOfInterest poi) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean deletePointOfInterest(PointOfInterest poi) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean updatePointOfInterest(PointOfInterest poi) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean addActivity(Activity a) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean deleteActivity(Activity a) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean updateActivity(Activity a) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean adminUpdateUser(User user) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	
+	
 }
