f005349db10e5e457fe6cbe6b6966e358861c261
Ryan Harrington <harrington.ryana@gmail.com>
2019-07-18 20:59:40 -0600

    Find event by id

--- Tripping/src/main/java/com/skilldistillery/tripping/data/EventDAOImpl.java
+++ Tripping/src/main/java/com/skilldistillery/tripping/data/EventDAOImpl.java
@@ -1,5 +1,50 @@
 package com.skilldistillery.tripping.data;
 
-public class EventDAOImpl {
+import java.util.List;
 
+import javax.persistence.*;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.tripping.entities.Event;
+
+@Transactional
+@Service
+public class EventDAOImpl implements EventDAO{
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public Event findPointOfInterestById(int id) {
+		em.find(Event.class, id);
+		return null;
+	}
+
+	@Override
+	public Event createEvent(Event event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateEvent(int id, Event event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletePointOfInteres(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Event> getEventsByDestination(int destinationId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
